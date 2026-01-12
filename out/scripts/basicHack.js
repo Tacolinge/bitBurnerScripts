c9d968e417f2feb6ff8946082876fca8ab9ced03
Tacolinge <128391785+Tacolinge@users.noreply.github.com>
2026-01-12 20:45:50 +0100

    made basicHack script

--- out/scripts/basicHack.js
+++ out/scripts/basicHack.js
@@ -1,1 +1,18 @@
 /** @param {NS} ns */
+export async function main(ns) {
+    const hackTarget = ns.read("hackTarget.txt") ;
+
+    // Infinite loop that continously hacks/grows/weakens the target server
+    while (true) {
+        if (ns.getServerSecurityLevel(hackTarget) > ns.getServerMinSecurityLevel(hackTarget) + 2) {
+            await ns.weaken(hackTarget) ;
+            continue ;
+        }
+        else if (ns.getServerMoneyAvailable(hackTarget) <= ns.getServerMaxMoney(hackTarget) * 0.75) {
+            await ns.grow(hackTarget) ;
+        }
+        else {
+            await ns.hack(hackTarget) ;
+        }
+    }
+}
